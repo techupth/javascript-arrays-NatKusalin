e5e47a5be7523cc3db8a171ecd8dd317133c18a0
Kusalin <kusalin.kokhun@gmail.com>
2024-04-14 19:59:10 +0700

    Fsdbp3-nat

--- ex-4.js
+++ ex-4.js
@@ -12,3 +12,22 @@ const employees = [
 ];
 
 // Start coding here
+let alicia = {
+  name: "Alicia",
+  age: 29,
+  hobbies: ["Shopping", "Reading novels"],
+};
+
+let kody = {
+  name: "Kody",
+  age: 19,
+  hobbies: ["Computer games", "Wakeboard"],
+};
+
+employees.push(alicia, kody);
+
+console.log(employees[0]);
+console.log(employees[1]);
+console.log(employees[2]);
+console.log(employees[3]);
+console.log(employees.length);
